ca5e1cac4c6fa1a14435020574ec07bdfbcd4efd
Artyom <malkovartem10@gmail.com>
2023-11-26 19:39:30 +0300

    Рефакторинг исходного кода, отладка вывода

--- main.cpp
+++ main.cpp
@@ -6,14 +6,9 @@
 #include <thread>
 #include <chrono>
 #include "custom_mutex.h"
+#include <mutex>
 
 class philosophers_having_lunch {
-private:
-
-    struct philosopher {
-        std::future<void> fut;
-        std::string name;
-    };
 public:
     philosophers_having_lunch(std::vector<std::string>&& _vec_names) 
         : vec_names(std::move(_vec_names))  {
@@ -39,7 +34,7 @@ public:
         
     } 
 
-    ~philosophers_having_lunch() {}
+    ~philosophers_having_lunch() = default;
 private:
 
     void exec(int _i_th) {
@@ -56,20 +51,25 @@ private:
 
     void eating(const std::string& _name) {
         using namespace std::chrono_literals;
+        mtx_output.lock();
         std::cout << _name << " eating..." << std::endl;
+        mtx_output.unlock();
+
         std::this_thread::sleep_for(5'000ms);
-        std::cout << _name << " stop eating" << std::endl;
         
+        mtx_output.lock();
+        std::cout << _name << " stop eating" << std::endl;
+        mtx_output.unlock();
     }
 
     
-    std::pair<size_t, size_t> getIndexFork(size_t _idx) {
+    std::pair<size_t, size_t> get_index_fork(size_t _idx) {
         return std::make_pair(_idx % vec_forks.size(),  
                                         (_idx + 1) % vec_forks.size());
     }
   
     bool take_possession_fork(size_t _idx) {
-        auto [lFork, rFork] = getIndexFork(_idx);
+        auto [lFork, rFork] = get_index_fork(_idx);
         if (!vec_forks[lFork]->is_locked() && !vec_forks[rFork]->is_locked()) {
             {
                 std::mutex mtx;
@@ -92,39 +92,32 @@ private:
     
     }  
 
-private:   
-    std::mutex main_mtx;
+private:  
     clock_t m_finish;
     std::vector<std::unique_ptr<custom_mutex>> vec_forks;
     std::vector<std::future<void>> vec_philosophers;
     std::vector<std::string> vec_names; 
+
+    
+    std::mutex mtx_output;
 };
 
 std::vector<std::string> init_names(size_t _n) {
     std::vector<std::string> vec_names;
     vec_names.reserve(_n);
     for(size_t i = 0; i < _n; ++i) {
-        vec_names.emplace_back(std::string("guy") + std::to_string(i + 1));
+        vec_names.emplace_back(std::string("guy_") + std::to_string(i + 1));
     }
     return vec_names;
 } 
 
 
 
-int main() {
+int main() {  
     int count_guys; 
     std::cout << "Enter the number of philosophers: ";
     std::cin >> count_guys;
     philosophers_having_lunch alg(init_names(count_guys));
-    
-    
-
-
-    
-
-
-
-
 
     return 0;
 }
